bc0fe8922eabc7db4e5f151971aab1732c57bb62
Isac Haran de Almeida <isac.almeida@unoesc.edu.br>
2019-11-07 00:44:39 -0300

    Ajustes nos pom

--- admin-server-parent/admin-server-application/src/main/java/br/edu/unoesc/adminserver/application/boot/AdminServerApplication.java
+++ admin-server-parent/admin-server-application/src/main/java/br/edu/unoesc/adminserver/application/boot/AdminServerApplication.java
@@ -2,9 +2,6 @@ package br.edu.unoesc.adminserver.application.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,17 +11,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @EnableAutoConfiguration
 @EnableAdminServer
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = AdminServerApplication.BASE_PACKAGE)
-public class AdminServerApplication extends SpringBootServletInitializer {
+public class AdminServerApplication {
 
 	public static final String BASE_PACKAGE = "br.edu.unoesc";
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AdminServerApplication.class);
 	}
 }
